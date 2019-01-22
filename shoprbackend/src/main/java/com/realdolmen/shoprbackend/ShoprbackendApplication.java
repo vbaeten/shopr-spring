38111d065b56d56caf4a6909ed61aba37e2b9857
MEETCH <mitch.demeulenaere@realdolmen.com>
2019-01-22 11:27:35 +0100

    first commit

---
+++ shoprbackend/src/main/java/com/realdolmen/shoprbackend/ShoprbackendApplication.java
@@ -0,0 +1,14 @@
+package com.realdolmen.shoprbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoprbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoprbackendApplication.class, args);
+    }
+
+}
+
