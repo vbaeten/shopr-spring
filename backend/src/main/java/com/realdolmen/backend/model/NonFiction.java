e80c1ea6fd1b281c547219922a72dc33a6739661
MeyersK <koenmeyers81@gmail.com>
2019-01-22 13:31:44 +0100

    model non fiction + enum

--- backend/src/main/java/com/realdolmen/backend/model/NonFiction.java
+++ backend/src/main/java/com/realdolmen/backend/model/NonFiction.java
@@ -3,14 +3,21 @@ package com.realdolmen.backend.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Data
-public class NonFiction
+public class NonFiction implements Serializable
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    NonFictionGenreEnum nonFictionGenreEnum;
+
+
+
 
 }
---
+++ backend/src/main/java/com/realdolmen/backend/model/NonFictionGenreEnum.java
@@ -0,0 +1,7 @@
+package com.realdolmen.backend.model;
+
+public enum NonFictionGenreEnum
+{
+RELIGION, BIOGRAPHY, FOOD, TRAVEL
+
+}
