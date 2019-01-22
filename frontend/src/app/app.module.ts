d082d67e428a1e8ce1b9f7fafafce0d40a6bc780
Inesvangucht <inesvangucht@gmail.com>
2019-01-22 17:24:51 +0100

    end of day

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
-import { FooterComponent } from './components/layout/header/footer/footer.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
