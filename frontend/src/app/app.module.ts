ca2e71afb8ad570cc1412a653d646b123ceaeda7
Inesvangucht <inesvangucht@gmail.com>
2019-01-22 16:40:09 +0100

    setup domain

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './components/app-footer/app-footer.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { AppNavbarComponent } from './components/app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFooterComponent,
+    AppHeaderComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
