7c459ac2951e1d7beca8149cefb0d410a73d2d30
MeyersK <koenmeyers81@gmail.com>
2019-01-22 14:17:18 +0100

    domain article

--- backend/src/main/java/com/realdolmen/backend/model/Article.java
+++ backend/src/main/java/com/realdolmen/backend/model/Article.java
@@ -3,18 +3,32 @@ package com.realdolmen.backend.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import java.io.Serializable;
 
 import static javax.persistence.DiscriminatorType.STRING;
 
 @Entity
 @Data
 @DiscriminatorColumn(discriminatorType = STRING , name = "type")
-public class Article
+public class Article implements Serializable
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
+    @NotBlank
+    private String title;
+    @NotBlank
+    @Digits(integer=6, fraction = 2)
+    private int price;
+    private String distributor;
+    @Column(insertable = false, updatable = false)
+    private String type;
+
 
 
 }
