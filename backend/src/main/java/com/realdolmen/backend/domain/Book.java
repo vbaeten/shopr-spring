8d51fb3b26224830b9a2cadd4f6637694da1c814
STABM42 <seid.tabakovic@realdolmen.com>
2019-01-23 16:14:56 +0100

    Adding Product repository

--- backend/src/main/java/com/realdolmen/backend/domain/Book.java
+++ backend/src/main/java/com/realdolmen/backend/domain/Book.java
@@ -3,14 +3,10 @@ package com.realdolmen.backend.domain;
 
 import lombok.Data;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @Data
-@DiscriminatorColumn(name = "book_type")
-@DiscriminatorValue("book")
 public abstract class Book extends Product {
 
     private String author;
