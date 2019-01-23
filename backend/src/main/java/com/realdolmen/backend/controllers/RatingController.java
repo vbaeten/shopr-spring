b14b3f9724cd3223f4b24e7be079d790cff5c56c
MeyersK <koenmeyers81@gmail.com>
2019-01-23 10:36:53 +0100

    ratingcontroller all/findone/save

---
+++ backend/src/main/java/com/realdolmen/backend/controllers/RatingController.java
@@ -0,0 +1,46 @@
+package com.realdolmen.backend.controllers;
+
+import com.realdolmen.backend.dao.FictionDao;
+import com.realdolmen.backend.dao.RatingDao;
+import com.realdolmen.backend.model.Fiction;
+import com.realdolmen.backend.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/rating")
+public class RatingController
+{
+    @Autowired
+    RatingDao ratingDao;
+
+    @Autowired
+    FictionDao fictionDao;
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Rating> getAllComponents()
+    {
+        return ratingDao.findAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Rating getOneForId(@PathVariable Long id)
+    {
+        return ratingDao.getOne(id);
+    }
+
+    @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Rating save(@RequestBody @Valid Rating rating)
+    {
+        return ratingDao.save( rating);
+    }
+
+}
