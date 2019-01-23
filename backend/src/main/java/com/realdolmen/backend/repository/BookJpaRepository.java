709880a088c0bf59ecb2ab036e23180e08dc7702
STABM42 <seid.tabakovic@realdolmen.com>
2019-01-23 16:41:08 +0100

    Adding all repositories

--- backend/src/main/java/com/realdolmen/backend/repository/BookJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.realdolmen.backend.repository;
-
-import com.realdolmen.backend.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookJpaRepository extends JpaRepository<Book, Long> {
-}
--- backend/src/main/java/com/realdolmen/backend/repository/ProductJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.realdolmen.backend.repository;
-
-import com.realdolmen.backend.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductJpaRepository extends JpaRepository<Product, Long> {
-}
