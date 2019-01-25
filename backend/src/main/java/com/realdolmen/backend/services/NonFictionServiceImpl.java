3b7a899859bdb02a77cd2219daa22ac95a99779f
MeyersK <koenmeyers81@gmail.com>
2019-01-25 15:47:30 +0100

    nonfictiongenre switch

--- backend/src/main/java/com/realdolmen/backend/services/NonFictionServiceImpl.java
+++ backend/src/main/java/com/realdolmen/backend/services/NonFictionServiceImpl.java
@@ -3,6 +3,8 @@ package com.realdolmen.backend.services;
 import com.realdolmen.backend.dao.NonFictionDao;
 import com.realdolmen.backend.exceptions.NotFoundException;
 import com.realdolmen.backend.model.NonFiction;
+import com.realdolmen.backend.model.enums.GameGenreEnum;
+import com.realdolmen.backend.model.enums.NonFictionGenreEnum;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,6 +32,23 @@ public class NonFictionServiceImpl
 
     public NonFiction save(NonFiction nonFiction)
     {
+        switch (nonFiction.getGenre())
+        {
+
+            case "religion":
+                nonFiction.setNonFictionGenreEnum(NonFictionGenreEnum.RELIGION);
+                break;
+            case "biography":
+               nonFiction.setNonFictionGenreEnum(NonFictionGenreEnum.BIOGRAPHY);
+                break;
+            case "food":
+                nonFiction.setNonFictionGenreEnum(NonFictionGenreEnum.FOOD);
+                break;
+            case "travel":
+               nonFiction.setNonFictionGenreEnum(NonFictionGenreEnum.TRAVEL);
+                break;
+
+        }
 
         return nonFictionDao.save(nonFiction);
     }
