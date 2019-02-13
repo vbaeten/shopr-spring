f62a3d4d6ddeefdb098ca0046c9a9d5d166fd1b3
MeyersK <koenmeyers81@gmail.com>
2019-02-13 15:22:07 +0100

    orderline service

--- backend/src/main/java/com/realdolmen/backend/services/OrderlineServiceImpl.java
+++ backend/src/main/java/com/realdolmen/backend/services/OrderlineServiceImpl.java
@@ -1,15 +1,23 @@
 package com.realdolmen.backend.services;
 
+import com.realdolmen.backend.dao.OrderlineDao;
+import com.realdolmen.backend.model.OrderLine;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderlineServiceImpl
 {
-    private Order
-    public User save(User user)
+    private OrderlineDao orderlineDao;
+
+    public OrderlineServiceImpl(OrderlineDao orderlineDao)
     {
-        user.setUserRoleEnum(UserRoleEnum.USER);
-        return userDao.save(user);
+        this.orderlineDao = orderlineDao;
+    }
+
+    public OrderLine save(OrderLine orderLine)
+    {
+
+        return orderlineDao.save(orderLine);
     }
 
 }
