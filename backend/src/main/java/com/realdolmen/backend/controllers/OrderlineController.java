9a8f33be1b9a57b308181f58ace4aa437565f4f1
MeyersK <koenmeyers81@gmail.com>
2019-03-06 13:46:23 +0100

    orderservice frontend

--- backend/src/main/java/com/realdolmen/backend/controllers/OrderlineController.java
+++ backend/src/main/java/com/realdolmen/backend/controllers/OrderlineController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/orderline")
 public class OrderlineController
 {
 
